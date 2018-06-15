47e9230122f4807dedf207abc5a7ed214bf958ec
Tomasz Kopacki <tomasz@kopacki.eu>
2018-06-15 21:42:02 +0200

    .

---
+++ mjs_fs/api_relay.js
@@ -0,0 +1,69 @@
+load('api_config.js');
+load('api_string.js');
+load('api_gpio.js');
+load('api_rpc.js');
+
+let relay = {
+    init(pin) {
+        GPIO.set_mode(pin, GPIO.MODE_OUTPUT);
+    },
+    on(pin) {
+        GPIO.write(pin, Cfg.get('relay.config.stateOn'));
+        print('Channel', name, 'switched to ON');
+    },
+    off(pin) {
+        GPIO.write(pin, Cfg.get('relay.config.stateOff'));
+        print('Channel', name, 'switched to OFF');
+    }
+};
+
+let module = {
+    id: Cfg.get('module.id'),
+    name: Cfg.get('module.name'),
+    items: {}
+}
+
+(function init() {
+    let enabledChannels = Cfg.get('relay.channels.enabled');
+    let enabledChannelsArray = StringUtils.split(enabledChannels, ',');
+    print('Enabled channels:', enabledChannelsArray.length);
+    for (let idx = 0; idx < enabledChannelsArray.length; idx++) {
+        module.items[enabledChannelsArray[idx]].id = Cfg.get(enabledChannelsArray[idx]);
+        module.items[enabledChannelsArray[idx]].name = Cfg.get(enabledChannelsArray[idx] + '.name');
+        module.items[enabledChannelsArray[idx]].pin = Cfg.get(enabledChannelsArray[idx] + '.pin');
+        module.items[enabledChannelsArray[idx]].type = 'switch';
+        module.items[enabledChannelsArray[idx]].state = 0;
+        relay.off(module.items[enabledChannelsArray[idx]].pin);
+    }
+    print('All channels initialized');
+})();
+
+(function registerRPCs() {
+    RPC.addHandler('Module.describe', function (args) {
+        return module;
+    });
+
+    RPC.addHandler('Module.itemState', function (args) {
+        return {
+            state: module.items[args.id].state
+        };
+    });
+
+    RPC.addHandler('Module.turnOn', function (args) {
+        relay.on(module.items[args.id].pin);
+        module.items[args.id].lastUpdate = new Date();
+        state: module.items[args.id].state = 1;
+        return {
+            result: '200'
+        };
+    });
+
+    RPC.addHandler('Module.turnOff', function (args) {
+        relay.off(module.items[args.id].pin);
+        module.items[args.id].lastUpdate = new Date();
+        state: module.items[args.id].state = 0;
+        return {
+            result: '200'
+        };
+    });
+})();
