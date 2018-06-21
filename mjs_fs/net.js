d7514e6380e54328308fb7c6d4d1fa05ecf7c6d1
Tomasz Kopacki <tomasz@kopacki.eu>
2018-06-21 15:01:12 +0200

    .

--- mjs_fs/net.js
+++ mjs_fs/net.js
@@ -1,22 +1,18 @@
 load('api_net.js');
 load('api_timer.js');
-load('api_events.js')
 
 print(Net.STATUS_DISCONNECTED);
 print(Net.STATUS_CONNECTING);
 print(Net.STATUS_CONNECTED );
 print(Net.STATUS_GOT_IP );
 
-Event.addGroupHandler(Net.EVENT_GRP, function(event, wtf, data) {
-    print(event);
-    print(data);
-}, {});
-
 function startServer() {
     Net.serve({
         addr: 'udp://11345',
-        onconnect: function (connection) {
-            print('Connection to broadcast established');
+        onevent: function (connection, data, ev, edata) {
+            print(data);
+            print(ev);
+            print(edata);
             Timer.set(5000, true, function () {
                 Net.send(connection, 'hello !');
                 print('Hello sent.');
