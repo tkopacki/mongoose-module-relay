fd326e203657c4750611b1484bacecec7421f973
Tomasz Kopacki <tomasz@kopacki.eu>
2018-06-22 11:10:59 +0200

    .

--- mjs_fs/net.js
+++ mjs_fs/net.js
@@ -11,28 +11,40 @@ let connection = Net.connect({
 });
 
 function startServer() {
-    /*     Net.serve({
-            addr: 'udp://11345',
-            onconnect: function (conn) {
-                print('connect');
-            },
-            ondata: function (conn, data) {
-                print('data');
-            },
-            onevent: function (conn, data, ev, edata) {
-                print('event');
-            },
-            onclose: function (conn) {
-                print('close');
-            },
-            onerror: function (conn) {
-                print('error');
-            }
-        }); */
 
     Timer.set(5000, Timer.REPEAT, function () {
         Net.send(connection, "aaa");
         print('sent');
+    }, null);
+
+    Event.addGroupHandler(Event.SYS, function (ev, evdata, ud) {
+        if (ev === Net.STATUS_GOT_IP) {
+            RPC.call(RPC.LOCAL, 'Sys.GetInfo', null, function (resp, ud) {
+                print('info:', JSON.stringify(resp));
+                let ip = resp.wifi.sta_ip;
+                let broadcast = 'udp://' + ip[0.10] + '255' + '11345'
+                print(ip);
+                print(broadcast);
+                Net.serve({
+                    addr: broadcast,
+                    onconnect: function (conn) {
+                        print('connect');
+                    },
+                    ondata: function (conn, data) {
+                        print('data');
+                    },
+                    onevent: function (conn, data, ev, edata) {
+                        print('event');
+                    },
+                    onclose: function (conn) {
+                        print('close');
+                    },
+                    onerror: function (conn) {
+                        print('error');
+                    }
+                });
+            }, null);
+        }
     }, null);
 
 }
