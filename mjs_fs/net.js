cf38790c528789d952827b655cbe8ca02be4a88f
Tomasz Kopacki <tomasz@kopacki.eu>
2018-06-22 12:06:38 +0200

    .

--- mjs_fs/net.js
+++ mjs_fs/net.js
@@ -7,30 +7,33 @@ print(Net.STATUS_CONNECTED);
 print(Net.STATUS_GOT_IP);
 
 let bag = {};
+
 function startServer() {
-/*     Net.serve({
-        addr: 'udp://192.168.0.255:11345',
-        onconnect: function (conn) {
-            print(conn, 'connect');
-        },
-        ondata: function (conn, data) {
-            print(conn, data);
-        },
-        onevent: function (conn, data, ev, edata) {
-            print('event');
-        },
-        onclose: function (conn) {
-            print(conn, 'close');
-        },
-        onerror: function (conn) {
-            print('error');
-        }
-    }); */
-    bag.c = Net.connect({
-        addr: 'udp://192.168.0.255:11345'
-    });
+    /*     Net.serve({
+            addr: 'udp://192.168.0.255:11345',
+            onconnect: function (conn) {
+                print(conn, 'connect');
+            },
+            ondata: function (conn, data) {
+                print(conn, data);
+            },
+            onevent: function (conn, data, ev, edata) {
+                print('event');
+            },
+            onclose: function (conn) {
+                print(conn, 'close');
+            },
+            onerror: function (conn) {
+                print('error');
+            }
+        }); */
+
     Timer.set(5000, Timer.REPEAT, function () {
-        Net.send(bag.c, "eee");
+        let c = Net.connect({
+            addr: 'udp://192.168.0.255:11345'
+        });
+        Net.send(c, "eee");
         print(bag.c, 'sent');
+        Net.close(c);
     }, null);
 }
