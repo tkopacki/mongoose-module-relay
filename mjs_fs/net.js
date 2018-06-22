c2002ab412e19a5274757257bf751c73a1f2a4ed
Tomasz Kopacki <tomasz@kopacki.eu>
2018-06-22 10:20:15 +0200

    .

--- mjs_fs/net.js
+++ mjs_fs/net.js
@@ -3,24 +3,28 @@ load('api_timer.js');
 
 print(Net.STATUS_DISCONNECTED);
 print(Net.STATUS_CONNECTING);
-print(Net.STATUS_CONNECTED );
-print(Net.STATUS_GOT_IP );
+print(Net.STATUS_CONNECTED);
+print(Net.STATUS_GOT_IP);
+
+let connectionPlaceHolder = {};
 
 function startServer() {
     Net.serve({
         addr: 'udp://11345',
-        onevent: function (connection, data, ev, edata) {
-            print(data);
-            print(ev);
-            print(edata);
-            Timer.set(5000, true, function () {
-                Net.send(connection, 'hello !');
-                print('Hello sent.');
-            }, null);
+        onconnect: function (conn) {
+            print('connect');
         },
-        ondata: function (connection, data) {
-            print(data);
-            print(connection);
+        ondata: function (conn, data) {
+            print('data');
+        },
+        onevent: function (conn, data, ev, edata) {
+            print('event');
+        },
+        onclose: function (conn) {
+            print('close');
+        },
+        onerror: function (conn) {
+            print('error');
         }
     });
 }
