da0f019ea5c3536cf0bc9ec0b16f18fe60bbf12f
Tomasz Kopacki <tomasz@kopacki.eu>
2018-06-22 11:56:14 +0200

    .

--- mjs_fs/net.js
+++ mjs_fs/net.js
@@ -6,23 +6,30 @@ print(Net.STATUS_CONNECTING);
 print(Net.STATUS_CONNECTED);
 print(Net.STATUS_GOT_IP);
 
+let bag = {};
 function startServer() {
     Net.serve({
         addr: 'udp://192.168.0.255:11345',
         onconnect: function (conn) {
-            print('connect');
+            print(conn, 'connect');
         },
         ondata: function (conn, data) {
-            print('data');
+            print(conn, data);
         },
         onevent: function (conn, data, ev, edata) {
             print('event');
         },
         onclose: function (conn) {
-            print('close');
+            print(conn, 'close');
         },
         onerror: function (conn) {
             print('error');
         }
     });
+    bag.c = Net.connect({
+        addr: 'udp://192.168.0.255:11345'
+    });
+    Timer.set(5000, Timer.REPEAT, function () {
+        Net.send(bag.c, "eee");
+    }, null);
 }
