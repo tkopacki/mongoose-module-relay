e262a545e12531dbf64d3fecc5ea05955dc68213
Tomasz Kopacki <tomasz@kopacki.eu>
2018-06-22 11:13:40 +0200

    .

--- mjs_fs/net.js
+++ mjs_fs/net.js
@@ -19,32 +19,24 @@ function startServer() {
 
     Event.addGroupHandler(Event.SYS, function (ev, evdata, ud) {
         if (ev === Net.STATUS_GOT_IP) {
-            RPC.call(RPC.LOCAL, 'Sys.GetInfo', null, function (resp, ud) {
-                print('info:', JSON.stringify(resp));
-                let ip = resp.wifi.sta_ip;
-                let broadcast = 'udp://' + ip[0.10] + '255' + ':11345'
-                print(broadcast === 'udp://192.168.0.255:11345');
-                print(ip);
-                print(broadcast);
-                Net.serve({
-                    addr: broadcast,
-                    onconnect: function (conn) {
-                        print('connect');
-                    },
-                    ondata: function (conn, data) {
-                        print('data');
-                    },
-                    onevent: function (conn, data, ev, edata) {
-                        print('event');
-                    },
-                    onclose: function (conn) {
-                        print('close');
-                    },
-                    onerror: function (conn) {
-                        print('error');
-                    }
-                });
-            }, null);
+            Net.serve({
+                addr: 'udp://255.255.255.255:11345',
+                onconnect: function (conn) {
+                    print('connect');
+                },
+                ondata: function (conn, data) {
+                    print('data');
+                },
+                onevent: function (conn, data, ev, edata) {
+                    print('event');
+                },
+                onclose: function (conn) {
+                    print('close');
+                },
+                onerror: function (conn) {
+                    print('error');
+                }
+            });
         }
     }, null);
 
